789234fdf3ab33b02d207926ac77a5047049e0b8
RAFI ODY PRASETYO <53549077+rafiody16@users.noreply.github.com>
2025-09-26 10:19:18 +0700

    CODELAB04
    CODELAB04

--- src/praktikum5.dart
+++ src/praktikum5.dart
@@ -11,20 +11,24 @@ void main() {
     return (b, a);
   }
 
+  var swapped = tukar((10, 20));
+  print('Sebelum tukar: (10, 20)');
+  print('Sesudah tukar: $swapped');
+
+  // Langkah 4
   print('Langkah 4');
-  // (String, int) mahasiswa;
-  // print(mahasiswa);
   (String, int) mahasiswa = ('Rafi Ody Prasetyo', 2341720180);
 
   print(mahasiswa);
-  print(mahasiswa.$1);
-  print(mahasiswa.$2);
+  print('Nama: ${mahasiswa.$1}');
+  print('NIM : ${mahasiswa.$2}');
 
+  // Langkah 5
   print('Langkah 5');
   var mahasiswa2 = ('first', a: 2, b: true, 'last');
 
   print(mahasiswa2.$1); // Prints 'first'
-  print(mahasiswa2.a); // Prints 2
-  print(mahasiswa2.b); // Prints true
+  print(mahasiswa2.a);  // Prints 2
+  print(mahasiswa2.b);  // Prints true
   print(mahasiswa2.$2); // Prints 'last'
 }
