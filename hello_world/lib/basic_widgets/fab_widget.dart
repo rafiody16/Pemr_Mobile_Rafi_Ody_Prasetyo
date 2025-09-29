c173078d5f8dcd7080a312bcb4456ccc75b57564
RAFI ODY PRASETYO <53549077+rafiody16@users.noreply.github.com>
2025-09-29 16:29:58 +0700

    CODELAB05
    CODELAB05

---
+++ hello_world/lib/basic_widgets/fab_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class FabWidget extends StatelessWidget {
+  const FabWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.thumb_up),
+          backgroundColor: Colors.pink,
+        ),
+      ),
+    );
+  }
+}
