391bd533c085a826ac85cef714ff3b8894eb2b84
RAFI ODY PRASETYO <53549077+rafiody16@users.noreply.github.com>
2025-11-26 11:19:44 +0700

    Praktikum 3

--- src/store_data_ody/lib/model/pizza.dart
+++ src/store_data_ody/lib/model/pizza.dart
@@ -1,28 +1,33 @@
 class Pizza {
-  final int id;
-  final String pizzaName;
-  final String description;
-  final double price;
-  final String imageUrl;
+  static const String keyId = 'id';
+  static const String keyName = 'pizzaName';
+  static const String keyDescription = 'description';
+  static const String keyPrice = 'price';
+  static const String keyImage = 'imageUrl';
 
-  Pizza.fromJson(Map<String, dynamic> json)
-    : id = int.tryParse(json['id'].toString()) ?? 0,
-      pizzaName = json['pizzaName'] != null
-          ? json['pizzaName'].toString()
-          : "No Name",
-      description = (json['description'] != null)
-          ? json['description'].toString()
-          : '',
-      price = double.tryParse(json['price'].toString()) ?? 0,
-      imageUrl = json['imageUrl'] ?? '';
+  int id = 0;
+  String pizzaName = '';
+  String description = '';
+  double price = 0.0;
+  String imageUrl = '';
+
+  Pizza();
+
+  Pizza.fromJson(Map<String, dynamic> json) {
+    id = int.tryParse(json[keyId].toString()) ?? 0;
+    pizzaName = json[keyName]?.toString() ?? 'No name';
+    description = json[keyDescription]?.toString() ?? '';
+    price = double.tryParse(json[keyPrice].toString()) ?? 0.0;
+    imageUrl = json[keyImage]?.toString() ?? '';
+  }
 
   Map<String, dynamic> toJson() {
     return {
-      'id': id,
-      'pizzaName': pizzaName,
-      'description': description,
-      'price': price,
-      'imageUrl': imageUrl,
+      keyId: id,
+      keyName: pizzaName,
+      keyDescription: description,
+      keyPrice: price,
+      keyImage: imageUrl,
     };
   }
 }
